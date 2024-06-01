01e496b2178bd1808f610e16f45b502d726ba723
Sushant Butta <sushant.butta@gmail.com>
2024-06-01 23:57:09 +0530

    changed

--- src/main/java/org/buttas/Main.java
+++ src/main/java/org/buttas/Main.java
@@ -1,7 +1,55 @@
 package org.buttas;
 
+class Base {
+    int x;
+
+    public Base() {
+        System.out.println("In Base");
+    }
+
+    public Base(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
+
+class Derived extends Base{
+    int y;
+
+    public Derived() {
+        System.out.println("In Derived");
+    }
+
+    public Derived(int y) {
+        this.y = y;
+        System.out.println("In Derived");
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Derived d = new Derived();
+        d.setX(5);
+        System.out.println(d.getX());
+
+        d.setY(15);
+        System.out.println(d.getY());
+
+        Derived d1 = new Derived(100);
+
+    };;
 }
